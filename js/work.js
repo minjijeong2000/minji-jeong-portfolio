c30ae8a4372bcca8db41634b0bef47bb59a1c787
Minji <dudvnd13@gmail.com>
2022-06-30 08:10:14 -0700

    upload new project

--- js/work.js
+++ js/work.js
@@ -1,10 +1,18 @@
-let projectData = {
+let projectData = [{
     title:'tic-tac-toe',
     deployment: 'https://ttt-weekend-minji.netlify.app',
     image: './images/tic-tac-toe.png',
     github: 'https://github.com/minjijeong2000/ttt-weekend',
     description: 'Want to play tic-tac-toe? Click the link below!'
-}
+},
+{
+    title:'marvel-app',
+    deployment: 'https://minji-jeong-marvel-app.herokuapp.com/',
+    image:'./images/my-app-minji.png',
+    github: 'https://github.com/minjijeong2000/minji-jeong-application',
+    description: 'Want to check out my marvel app? Click the link below!'
+
+}]
 
 export {
     projectData
